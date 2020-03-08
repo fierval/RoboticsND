5e1439bcf4ee6b41959def44212a6c2c08cfe94e
Boris Kogan <fierval@gmail.com>
2020-03-08 00:03:16 -0800

    refactoring

--- Project1/script/hello.cpp
+++ Project1/script/hello.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginRobot() : WorldPlugin()
             {
-              printf("Hello World!\n");
+              printf("Welcome to Boris's world!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
