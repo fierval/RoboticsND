7efc57c32a218984348f212193801e24c16d76e1
Boris Kogan <fierval@gmail.com>
2020-05-17 18:55:03 -0700

    add & to testslam

--- Project7/catkin_ws/src/scripts/testslam.sh
+++ Project7/catkin_ws/src/scripts/testslam.sh
@@ -3,4 +3,5 @@ xterm  -e  " roslaunch my_turtle turtlebot3_home_world.launch" &
 sleep 10
 xterm  -e  " roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping " &
 sleep 5
-xterm  -e  " roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch "
+xterm  -e  " roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch " &
+
