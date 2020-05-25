1be7c0022f24b1404195d9296a4870e838fc752f
Boris Kogan <fierval@gmail.com>
2020-05-25 15:38:33 -0700

    add markers working

--- Project7/catkin_ws/scripts/add_marker.sh
+++ Project7/catkin_ws/scripts/add_marker.sh
@@ -4,3 +4,12 @@ sleep 10
 xterm  -e  " roslaunch my_turtle turtlebot3_navigation.launch" &
 sleep 5
 xterm -e " rosrun add_markers add_markers_node" &
+rostopic pub -1 /goal_state pick_objects/PointReached '{name: set, x: 3.0, y: 4.0}'
+echo "sleeping for 2 sec"
+sleep 2
+rostopic pub -1 /goal_state pick_objects/PointReached '{name: pickup, x: 3.0, y: 4.0}'
+echo "sleeping for 2 sec"sleep 2
+rostopic pub -1 /goal_state pick_objects/PointReached '{name: set, x: -5.0, y: 1.0}'
+echo "sleeping for 2 sec"
+sleep 2
+rostopic pub -1 /goal_state pick_objects/PointReached '{name: dropoff, x: -5.0, y: 1.0}'
